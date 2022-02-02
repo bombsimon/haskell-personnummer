e7eb72d7a03c7c9648a612811624afa4540dd890
Simon Sawert <simon@sawert.se>
2022-02-02 21:51:32 +0100

    Add support to format and get age

--- src/Personnummer.hs
+++ src/Personnummer.hs
@@ -1,13 +1,17 @@
 module Personnummer (isValid, toPersonnummer) where
 
-import Data.Maybe (fromMaybe, isNothing)
+import Control.Monad
+import Data.Maybe (fromJust, fromMaybe, isNothing)
 import Data.Time
   ( UTCTime,
     defaultTimeLocale,
     formatTime,
+    getCurrentTime,
     parseTimeM,
     parseTimeOrError,
+    toGregorian,
   )
+import Data.Time.Clock (utctDay)
 import Text.Printf (printf)
 import Text.Read (readMaybe)
 import Text.Regex
@@ -15,8 +19,6 @@ import Text.Regex
     matchRegex,
     mkRegex,
   )
-
--- https://github.com/Tarrasch/threepenny-personnummer/blob/master/PersonNummer.hs
 
 {-
 Regex to match personal identity number. Used both to determine valid values but
@@ -33,6 +35,12 @@ data Gender
   | Female
   deriving (Eq, Show)
 
+{-
+Personnummber represents all the date for a personal identity number. The birth
+date is represented as a UTCTime for easier handling. Will be Nothing if not a
+valid date. Even if the Personnummer is a coordination number we store a proper
+date and set isCoordination to True instead.
+-}
 data Personnummer = Personnummer
   { date :: Maybe UTCTime,
     number :: Int,
@@ -40,6 +48,68 @@ data Personnummer = Personnummer
     divider :: Char,
     isCoordination :: Bool
   }
+  deriving (Eq, Show)
+
+{-
+Check if a Personnummber is valid. That is, if it is a valid date and the
+checksum adds up to the control digit.
+-}
+isValid :: Personnummer -> Bool
+isValid p
+  | isNothing (date p) = False
+  | luhnP p == control p = True
+  | otherwise = False
+
+{-
+Get the gender for the Personnummber.
+-}
+gender :: Personnummer -> Gender
+gender p = case number p `mod` 10 `mod` 20 of
+  0 -> Female
+  _ -> Male
+
+{-
+Check if the Personnummber belongs to a Female.
+-}
+isFemale :: Personnummer -> Bool
+isFemale p = gender p == Female
+
+{-
+Check if the Personnummber belongs to a Male.
+-}
+isMale :: Personnummer -> Bool
+isMale p = gender p == Male
+
+{-
+Get age pure doesn't calculate the current time but instead asks the caller to
+send a tuple with the current year, month and day. Based on that the functon
+will calculate the age.
+-}
+getAgePure :: Personnummer -> (Int, Int, Int) -> Int
+getAgePure p (y, m, d)
+  | isNothing $ date p = 0
+  | m > m2 = y - y2
+  | m == m2 && d >= d2 = y - y2
+  | otherwise = y - y2 - 1
+  where
+    (year, m2, d2) = toGregorian $ utctDay (fromJust $ date p)
+    y2 = fromIntegral year
+
+{-
+Format to consistent Personnummer output, either in long format (including
+century) or short format (just two digits)
+-}
+format :: Personnummer -> Bool -> String
+format p long =
+  t
+    ++ [divider p]
+    ++ printf "%03d" (number p)
+    ++ printf "%d" (control p)
+  where
+    f = if long then "%Y%m%d" else "%y%m%d"
+    t = case date p of
+      Just d -> formatTime defaultTimeLocale f d
+      Nothing -> if long then "00000000" else "000000"
 
 {-
 Construct a Personnummer from a string.
@@ -72,12 +142,6 @@ toPersonnummerFrommatches m =
   where
     x = map intOrZeroFromString m
     d = if m !! 4 == "+" then '+' else '-'
-
-isValid :: Personnummer -> Bool
-isValid p
-  | isNothing (date p) = False
-  | luhnP p == control p = True
-  | otherwise = False
 
 {-
 Try to read string as int and if it fails, return 0
@@ -119,9 +183,9 @@ luhnP p =
   case date p of
     Nothing -> -1
     Just t ->
-      luhn $ map (read . (: [])) $ d ++ printf "%03d" (number p)
-      where
-        d = formatTime defaultTimeLocale "%y%m%d" t
+      luhn $
+        map (read . (: [])) $
+          formatTime defaultTimeLocale "%y%m%d" t ++ printf "%03d" (number p)
 
 {-
 Use luhn algoritm to calculate the control digit.
